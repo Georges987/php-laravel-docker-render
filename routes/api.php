a1876cb22f18a22abf22d96094e5f6ac7b67372f
= <ayenigeorgepierre@gmail.com>
2024-11-11 18:11:38 +0100

    new user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json(['user' => "Hello new user"]);
+    //$request->user();
 });
+
